a0c75a3ea68012d3f79c8594bccb053978fe0fb4
codessathua <s.sathua@gmail.com>
2016-04-01 09:38:58 -0700

    Update TestWrap.java

--- TestWrap.java
+++ TestWrap.java
@@ -40,10 +40,8 @@ public class TestWrap {
     public void publishWrap()  {
         driver.findElement(By.cssSelector(" #wrap-theme > div.authoring-container > div:nth-child(1) > div > div > div.wraps > div.row > a")).click();
         driver.findElement(By.cssSelector("#wrap-theme > div.modal.fade.ng-isolate-scope.wrap_create-modal.in > div > div > div.modal-body.ng-scope > wm-template-selector > div.wrap_create-templates > div:nth-child(1) > div > button:nth-child(2)")).click();
-        //WebElement element = driver.findElement(By.cssSelector("#wrap-theme > div.authoring-container > div:nth-child(1) > div > div > div > card-header > div > div.card_header-top.clearfix > div.card_header-actions.card_header-actions--right.pull-right > button.btn.btn-success"));
         WebElement myElement = (new WebDriverWait(driver, 30))
                 .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#wrap-theme > div.authoring-container > div:nth-child(1) > div > div > div > card-header > div > div.card_header-top.clearfix > div.card_header-actions.card_header-actions--right.pull-right > button.btn.btn-success")));
-       // Thread.sleep(40000);
         System.out.println(driver.getTitle());
         driver.findElement(By.cssSelector("#wrap-theme > div.authoring-container > div:nth-child(1) > div > div > div > card-header > div > div.card_header-top.clearfix > div.card_header-actions.card_header-actions--right.pull-right > button.btn.btn-success")).click();
         System.out.println(driver.getTitle());
